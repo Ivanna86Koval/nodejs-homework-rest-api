49b9ff77066e3239e48341343ee0af97330687a1
Ivanna86Koval <ms.ivanna86@gmail.com>
2023-12-13 01:08:44 +0200

    fix user

---
+++ models/contacts/User.js
@@ -0,0 +1,48 @@
+import { Schema, model } from "mongoose";
+import Joi from "joi";
+import { handleSaveError, preUpdate } from "./hooks.js";
+
+const emailRegexp = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+
+const userSchema = new Schema(
+  {
+    password: {
+      type: String,
+      required: [true, "Set password for user"],
+      minLength: 6,
+    },
+    email: {
+      type: String,
+      required: [true, "Email is required"],
+      match: emailRegexp,
+      unique: true,
+    },
+    subscription: {
+      type: String,
+      enum: ["starter", "pro", "business"],
+      default: "starter",
+    },
+    token: {
+      type: String,
+    },
+  },
+  { versionKey: false }
+);
+
+userSchema.post("save", handleSaveError);
+userSchema.pre("findOneAndUpdate", preUpdate);
+userSchema.post("findOneAndUpdate", handleSaveError);
+
+export const userSignupSchema = Joi.object({
+  email: Joi.string().pattern(emailRegexp).required(),
+  password: Joi.string().min(6).required(),
+  subscription: Joi.string(),
+});
+export const userSigninSchema = Joi.object({
+  email: Joi.string().pattern(emailRegexp).required(),
+  password: Joi.string().min(6).required(),
+});
+
+const User = model("user", userSchema);
+
+export default User;
